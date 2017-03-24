a84f410c351ed01d889e28be01bd41f388426a4f
Vladimír Zdražil <mail@vzdrazil.com>
2017-03-24 09:24:47 +0100

    Add test format amount

--- test_my_parser_csv.py
+++ test_my_parser_csv.py
@@ -1,8 +1,11 @@
-from parser import *
+from my_parser_csv import *
+import locale
 import datetime
 
-def test_get_date_range():
-    
-    from_date = datetime.datetime.now() + datetime.timedelta(-int(20))
-    to_date = datetime.datetime.now
-    assert get_date_range(20) == (from_date,to_date)
+def test_format_amount():
+    locale.setlocale(locale.LC_ALL, 'cs_CZ')
+    formatted_amount = '225 500,50'
+    tested_amount = locale.format("%.2f", float(225500.50), grouping=True)
+    assert formatted_amount == tested_amount
+
+
