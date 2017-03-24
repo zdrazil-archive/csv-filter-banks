eb3ffd0257b4f6571c459da48b91b3902cf36a0b
Vladimír Zdražil <mail@vzdrazil.com>
2017-03-24 07:24:37 +0100

    Refactor name of the module parser.py > my_parser_csv

--- filter_cz.py
+++ filter_cz.py
@@ -1,29 +1,29 @@
-import parser
+import my_parser_csv
 
 
 def get_payments():
-    csv_reader = parser.get_csv_reader('windows-1250', ';', '"')
+    csv_reader = my_parser_csv.get_csv_reader('windows-1250', ';', '"')
     columns = {'date': 13,
                'name': 9,
                'var_symbol': 16,
                'amount': 3}
-    payments = parser.get_payments(columns, csv_reader, '%Y/%m/%d')
+    payments = my_parser_csv.get_payments(columns, csv_reader, '%Y/%m/%d')
     for payment in payments:
         payment['amount'] = float(payment['amount'].replace(',', '.'))
-    filtered_payments = parser.filter_payments(payments)
+    filtered_payments = my_parser_csv.filter_payments(payments)
 
     return filtered_payments
 
 
 def format_payments(payments):
     for payment in payments:
-        payment['date'] = parser.format_date(payment['date'])
-        payment['amount'] = parser.format_amount(payment['amount'])
+        payment['date'] = my_parser_csv.format_date(payment['date'])
+        payment['amount'] = my_parser_csv.format_amount(payment['amount'])
     return payments
 
 
 def create_file(payments, final_file):
-    parser.create_final_file(payments, final_file)
+    my_parser_csv.create_final_file(payments, final_file)
 
 
 payments = get_payments()
--- filter_sk.py
+++ filter_sk.py
@@ -1,27 +1,27 @@
-import parser
+import my_parser_csv
 
 
 def get_payments():
-    csv_reader = parser.get_csv_reader('utf-8', ';', '"')
+    csv_reader = my_parser_csv.get_csv_reader('utf-8', ';', '"')
     columns = {'date': 0,
                'name': 6,
                'var_symbol': 13,
                'amount': 7}
-    payments = parser.get_payments(columns, csv_reader, '%d.%m.%Y')
-    filtered_payments = parser.filter_payments(payments)
+    payments = my_parser_csv.get_payments(columns, csv_reader, '%d.%m.%Y')
+    filtered_payments = my_parser_csv.filter_payments(payments)
 
     return filtered_payments
 
 
 def format_payments(payments):
     for payment in payments:
-        payment['date'] = parser.format_date(payment['date'])
-        payment['amount'] = parser.format_amount(payment['amount'])
+        payment['date'] = my_parser_csv.format_date(payment['date'])
+        payment['amount'] = my_parser_csv.format_amount(payment['amount'])
     return payments
 
 
 def create_file(payments, final_file):
-    parser.create_final_file(payments, final_file)
+    my_parser_csv.create_final_file(payments, final_file)
 
 
 payments = get_payments()
